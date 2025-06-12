20a7dd375f840e6d8c6343e134b05240d5daf7ae
Yuginda <yugindaranawaka@gmail.com>
2025-06-11 18:35:08 -0700

    Added profile image and content to Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,17 @@
 import './Hero.css'
+import profile from '../../assets/profile.jpg'
 
 function Hero(){
 
     return(
-        <div>
-
+        <div className='hero'>
+            <img src={profile} alt="" />
+            <h1>Hi, I'm Yuginda Ranawaka</h1>
+            <p>I am a student from SFU, learning computer engineering</p>
+            <div className='action'>
+                <div>Connect</div>
+                <div>Resume</div>
+            </div>
         </div>
     )
 }
