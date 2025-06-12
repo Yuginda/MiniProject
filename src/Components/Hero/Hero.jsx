5cdbc1b5c26727de8adbc75aa4c036a71846d5ae
Yuginda <yugindaranawaka@gmail.com>
2025-06-11 22:09:55 -0700

    Added CSS propoties and alt text to profile imgage

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,15 +5,16 @@ function Hero(){
 
     return(
         <div className='hero'>
-            <img src={profile} alt="" />
+            <img src={profile} alt="Picture of me when I went rock climbing" />
             <h1>Hi, I'm Yuginda Ranawaka</h1>
             <p>I am a student from SFU, learning computer engineering</p>
             <div className='action'>
-                <div>Connect</div>
-                <div>Resume</div>
+                <div className='connect'>Connect</div>
+                <div className='resume'>Resume</div>
             </div>
         </div>
     )
 }
 
 export default Hero
+
