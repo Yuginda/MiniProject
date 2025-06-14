9313ca6c0e344d2c77e8a626647425bd82e991db
Yuginda <yugindaranawaka@gmail.com>
2025-06-14 16:13:45 -0700

    added span tag to h1 to make use of webkit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ function Hero(){
     return(
         <div className='hero'>
             <img src={profile} alt="Picture of me when I went rock climbing" />
-            <h1>I'm Yuginda Ranawaka, a student from SFU</h1>
+            <h1><span>I'm Yuginda Ranawaka,</span> a student from SFU</h1>
             <p>I am a passionate computer engineering student who is intrested in building web applications and finding a way to apply my skills in real-world applications.</p>
             <div className='action'>
                 <div className='connect'>Connect</div>
