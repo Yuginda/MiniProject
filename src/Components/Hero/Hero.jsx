7fdee5914a1abffb49ee4b1a88afbda0c8706cc5
Yuginda <yugindaranawaka@gmail.com>
2025-06-16 20:14:37 -0700

    Added id to help link home to hero in navigation bar

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import profile from '../../assets/profile.jpg'
 function Hero(){
 
     return(
-        <div className='hero'>
+        <div className='hero' id = 'hero'>
             <img src={profile} alt="Picture of me when I went rock climbing" />
             <h1><span>I'm Yuginda Ranawaka,</span> a student from SFU</h1>
             <p>I am a passionate computer engineering student who is intrested in building web applications and finding a way to apply my skills in real-world applications.</p>
