5c6b9e4d49c7c4ea3029dc31fd7889f19a3488e7
Yuginda <yugindaranawaka@gmail.com>
2025-06-17 02:31:58 -0700

    Added an arrow to go from footer to header

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,17 @@
+
 import './Footer.css'
 import signature from '../../assets/signature.jpg'
 
 function Footer() {
+  const scrollToTop = () =>
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+
   return (
     <footer className="footer">
       <img src={signature} alt="My signature which consists of a long R followed by the word yuginda" className="footer-signature"/>
+      <div className="back-to-top" onClick={scrollToTop}>
+        &uarr;
+      </div>
       <p>&copy; {new Date().getFullYear()} Yuginda Ranawaka. All rights reserved.</p>
     </footer>
   )
