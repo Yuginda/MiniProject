8c2b14550531142514f76eb1395fd1c42b88a7db
Yuginda <yugindaranawaka@gmail.com>
2025-06-17 00:22:13 -0700

    Added resume link and the connect link within the hero component

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,6 +2,16 @@ import './Hero.css'
 import profile from '../../assets/profile.jpg'
 
 function Hero(){
+    const scrollToSection = (sectionId) => {
+        const element = document.getElementById(sectionId);
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
+    const openResume = () => {
+        window.open('/Yugindacv.pdf', '_blank', 'noopener,noreferrer')
+    }
 
     return(
         <div className='hero' id = 'hero'>
@@ -9,12 +19,11 @@ function Hero(){
             <h1><span>I'm Yuginda Ranawaka,</span> a student from SFU</h1>
             <p>I am a passionate computer engineering student who is intrested in building web applications and finding a way to apply my skills in real-world applications.</p>
             <div className='action'>
-                <div className='connect'>Connect</div>
-                <div className='resume'>Resume</div>
+                <div className='connect' onClick={() => scrollToSection('contact')}>Connect</div>
+                <div className="resume" onClick={openResume}>Resume</div>
             </div>
         </div>
     )
 }
 
-export default Hero
-
+export default Hero 
