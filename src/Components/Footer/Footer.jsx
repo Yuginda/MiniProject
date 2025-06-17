ed72e21b95d001d1a6604b2d4b9fb350b0084687
Yuginda <yugindaranawaka@gmail.com>
2025-06-17 01:47:44 -0700

    Added signataure to assets folder and footer component

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,13 +1,13 @@
 import './Footer.css'
+import signature from '../../assets/signature.jpg'
 
-function Footer(){
-
-    return(
-
-        <div>
-            <h2>Footer</h2>
-        </div>
-    )
+function Footer() {
+  return (
+    <footer className="footer">
+      <img src={signature} alt="My signature which consists of a long R followed by the word yuginda" className="footer-signature"/>
+      <p>&copy; {new Date().getFullYear()} Yuginda Ranawaka. All rights reserved.</p>
+    </footer>
+  )
 }
 
 export default Footer
